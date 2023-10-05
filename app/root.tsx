081b6774bdb373f35cf2c237901c7d6ebcab1126
phbord <phbord@2cconseil.com>
2023-10-05 17:49:47 +0200

    29 fetching...

--- app/root.tsx
+++ app/root.tsx
@@ -1,11 +1,14 @@
 import type { LinksFunction } from "@remix-run/node";
 import {
+  Link,
   Links,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  isRouteErrorResponse,
+  useRouteError,
 } from "@remix-run/react";
 
 import styles from '~/styles/main.css'
@@ -33,4 +36,33 @@ export default function App() {
       </body>
     </html>
   );
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+        <title>An error occurred!</title>
+      </head>
+      <body>
+        <header>
+          <MainNavigation />
+        </header>
+        <main className="error">
+          <h1>An error occurred!</h1>
+          <p>{error.message}</p>
+          <p>Back to <Link to="/">safety</Link>!</p>
+        </main>
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  )
 }
--- app/routes/notes._index.tsx
+++ app/routes/notes._index.tsx
@@ -1,11 +1,12 @@
 import { json, redirect } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { Link, isRouteErrorResponse, useLoaderData, useRouteError } from "@remix-run/react";
 import NewNote, { links as newNoteLinks } from "~/components/NewNote";
 import NoteList, { links as noteListLinks } from "~/components/NoteList";
 import { getStoredNotes, storeNotes } from '~/data/notes';
 
 export default function NotesPage() {
   const notes = useLoaderData();
+  
   return (
     <main>
       <NewNote />
@@ -16,6 +17,16 @@ export default function NotesPage() {
 
 export async function loader() {
   const notes = await getStoredNotes();
+  if (!notes || notes.length === 0) {
+    throw json(
+      {
+        message: "Could not find any notes."
+      }, {
+        status: 404,
+        statusText: 'Not found'
+      }
+    );
+  }
   return json(notes);
 }
 
@@ -27,13 +38,40 @@ export async function action({request}) {
     content: formData.get('content'),
   }; */
   const noteData = Object.fromEntries(formData);
-  // Add validation...
+  
+  if (noteData.title.trim().length < 5) {
+    return {message: "Invalid title - must be at least 5 characteres long."}
+  }
+
   const existingNotes = await getStoredNotes();
   noteData.id = new Date().toISOString();
   const updatedNotes = existingNotes.concat(noteData);
   await storeNotes(updatedNotes);
   //await new Promise((resolve, reject) => setTimeout(() => resolve(), 2000))
   return redirect('/notes');
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  // Equivaut à `CatchBoundary`
+  if (isRouteErrorResponse(error)) {
+    const message = error.data?.message || 'Data not found.';
+    return(
+      <main>
+        <NewNote />
+        <p className="info-message">{message}</p>
+      </main>
+    )
+  }
+
+  return (
+    <main className="error">
+      <h1>An error related to your notes occurred!</h1>
+      <p>{error.message}</p>
+      <p>Back to <Link to="/">safety</Link>!</p>
+    </main>
+  )
 }
 
 export function Links() {
